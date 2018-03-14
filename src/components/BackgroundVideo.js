f3a6e42d5b3464dd856a792119349eb548684e27
juneyoungoh <xzawed85>
2018-03-15 00:40:22 +0900

    todo : override window.resize

--- src/components/BackgroundVideo.js
+++ src/components/BackgroundVideo.js
@@ -15,11 +15,12 @@ export default class BackgroundVideo extends Component {
 		/*
 			video 태그는 브라우저에 따라 호환이 다름 
 		*/
-
+		var backgroundVideoWidth = window.innerWidth;
+		var backgroundVideoHeight = 'auto'; // window.innerHeight;
 
 		return (
 			<div className="bg_video">
-				<video with='100%' height='auto;' autoplay='true'>
+				<video width={ backgroundVideoWidth } height={ backgroundVideoHeight } autoPlay='true'>
 					<source src="assets/media/dock.mp4" type="video/mp4"></source>
 				</video>
 			</div>
